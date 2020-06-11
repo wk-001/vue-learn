a52ad78ab4e9251642488dca6e030e6b622888b9
wk-001 <2326699650@qq.com>
2020-06-11 17:48:14 +0800

    循环

---
+++ 08-购物车案例/main.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        books:[
+            {name:'Java',date:'2001-1-1',price:20,quantity:1},
+            {name:'Python',date:'1996-1-1',price:30,quantity:1},
+            {name:'Kotlin',date:'2010-5-5',price:50,quantity:1},
+            {name:'Go',date:'200910-9',price:40,quantity:1}
+        ]
+    },
+    computed:{
+        totalPrice(){
+            let result = 0;
+            for (let book of this.books){
+                result += book.price * book.quantity;
+            }
+            return result;
+        }
+    },
+    methods:{
+        increase(index){
+            this.books[index].quantity++;
+        },
+        decrease(index){
+            this.books[index].quantity--;
+        },
+        removeBook(index){
+            this.books.splice(index,1)
+        }
+    },
+    filters:{
+        priceFilter(price){
+            return '￥' + price.toFixed(2);
+        }
+    }
+});
