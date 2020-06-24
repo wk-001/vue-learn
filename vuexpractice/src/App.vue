515de479233fe9da6a3239511e7e87596d14f6c7
wk-001 <2326699650@qq.com>
2020-06-24 17:50:24 +0800

    promise vuex

---
+++ vuexpractice/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+    <h2>{{counter}}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+
+    <hello-vuex :counter="counter"/>
+  </div>
+</template>
+
+<script>
+
+  import HelloVuex from "./components/HelloVuex";
+
+  export default {
+    name: 'App',
+    components:{
+      HelloVuex
+    },
+    data(){
+      return{
+        message:'vuex组件',
+        counter:0
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
